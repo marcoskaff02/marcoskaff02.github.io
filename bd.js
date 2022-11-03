c68cb565a630dca69fcc143290b3377b7bd6fb1e
marcoskaff02 <marcoskaff02@gmail.com>
2022-11-03 11:37:47 -0300

    Github enviar

---
+++ bd.js
@@ -0,0 +1,16 @@
+var bd = openDatabase("meuBD", "1.0", "Meu Banco de Dados", 4080);
+
+bd.transaction(function(criar){
+    criar.executeSql(
+        "CREATE TABLE nomes (nome TEXT)"
+    );
+});
+function salvarNome(){
+    const nomeUsuario = document.getElementById("nome-usuario").value;
+
+bd.transaction(function(inserir){
+    inserir.executeSql(
+        "INSERT INTO nomes (nome) VALUES (?)", [nomeUsuario]
+    );
+});
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+function converteParaFahrenheit(){
+    const temperaturaCelsius =  parseFloat(document.getElementById("temperatura-celsius").value); 
+
+   const temperaturaFahrenheit = temperaturaCelsius * 1.8 + 32;
+
+   document.getElementById("resultado-fahrenheit").value = 
+   temperaturaFahrenheit.toFixed(2);
+}
