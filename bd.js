ac5ef3cdf81827841c3394be8bf6b4ae7730fab3
marcoskaff02 <marcoskaff02@gmail.com>
2022-11-03 12:04:21 -0300

    Idade e nome

--- bd.js
+++ bd.js
@@ -1,16 +1,17 @@
 var bd = openDatabase("meuBD", "1.0", "Meu Banco de Dados", 4080);
 
 bd.transaction(function(criar){
-    criar.executeSql(
-        "CREATE TABLE nomes (nome TEXT)"
-    );
+    criar.executeSql("CREATE TABLE formulario (nome TEXT, idade INTEGER)");
 });
-function salvarNome(){
+function salvarInfo(){
     const nomeUsuario = document.getElementById("nome-usuario").value;
-
+    const idadeUsuario = parseInt(
+     document.getElementById("idade-usuario").value
+    );
 bd.transaction(function(inserir){
     inserir.executeSql(
-        "INSERT INTO nomes (nome) VALUES (?)", [nomeUsuario]
+        "INSERT INTO formulario (nome, idade) VALUES (?, ?)", 
+        [nomeUsuario, idadeUsuario]
     );
 });
 }
