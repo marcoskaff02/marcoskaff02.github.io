fdcd950536fc2aeabcef3c762ca64ee3172e0394
marcoskaff02 <marcoskaff02@gmail.com>
2022-11-08 11:44:03 -0300

    Inserção da Exibição do Banco de Dados

--- bd.js
+++ bd.js
@@ -4,15 +4,23 @@ bd.transaction(function(criar){
     criar.executeSql("CREATE TABLE formulario (nome TEXT, idade INTEGER)");
 });
 function salvarInfo(){
-    const nomeUsuario = document.getElementById("nome-usuario").value;
+    const nomeUsuario = document.getElementById("nome-usuario").value.toUpperCase();
     const idadeUsuario = parseInt(
      document.getElementById("idade-usuario").value
     );
+
+    if (nomeUsuario === "" || isNaN(idadeUsuario)){
+        alert("Faltam informações!");
+        return false;
+    }
+
 bd.transaction(function(inserir){
     inserir.executeSql(
         "INSERT INTO formulario (nome, idade) VALUES (?, ?)", 
         [nomeUsuario, idadeUsuario]
     );
+    document.getElementById("nome-usuario").value ="";
+    document.getElementById("idade-usuario").value ="";
 });
 }
 function pesquisaPorNome(){
@@ -23,6 +31,26 @@ function pesquisaPorNome(){
             const tamanho = resultados.rows.length;
             const msg = tamanho + "linhas encontradas";
             console.log(msg)
+
+            const nome = resultados.rows.item(tamanho - 1).nome;
+            const idade = resultados.rows.item(tamanho - 1).idade;
+            document.getElementById("pesquisa-nome-usuario").value = nome;
+            document.getElementById("resultado-pesquisa").value = idade;
         } )
+    })
+}
+function exibeBD(){
+    bd.transaction(function(exibe){
+        exibe.executeSql("SELECT * FROM formulario", [], function(exibe, resultados){
+            const tamanho = resultados.rows.length;
+            let item;
+
+            document.getElementById("lista-bd").innerHTML = "";
+
+            for (let i = 0; i < tamanho; i++){
+                item = resultados.rows.item(i);
+                document.getElementById("lista-bd").innerHTML += `<p>Nome: ${item.nome}, ${item.idade} anos</p>`;
+            }
+        })
     })
 }
